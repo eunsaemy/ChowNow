834c841e34d815c4779941dcf843e1d216aa0500
Christina <christinaraganit@gmail.com>
2023-03-11 21:34:11 -0800

    hewwo world

--- App.js
+++ App.js
@@ -1,33 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import LoginScreen from './screens/LoginScreen';
-import SignUp1Screen from './screens/SignUp1Screen';
-import SignUp2Screen from './screens/SignUp2Screen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import SignUp1Screen from "./screens/SignUp1Screen";
+import SignUp2Screen from "./screens/SignUp2Screen";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="SignUp1" component={SignUp1Screen} />
-                <Stack.Screen name="SignUp2" component={SignUp2Screen} />
-            
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  const [fontsLoaded] = useFonts({
+    "Karla-Bold": require("./assets/fonts/Karla-Bold.ttf"),
+    "Karla-Regular": require("./assets/fonts/Karla-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          style={styles.text}
+          options={{ headerShown: false }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          style={styles.text}
+        />
+        <Stack.Screen
+          name="SignUp1"
+          style={styles.text}
+          component={SignUp1Screen}
+        />
+        <Stack.Screen
+          styles="style.text"
+          name="SignUp2"
+          component={SignUp2Screen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fafafa",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontFamily: "Karla-Regular",
+  },
 });
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,134 +1,176 @@
-import React, { useEffect, useState } from 'react'
-import { auth, db } from '../firebase'
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth'
-import { collection, doc, getDoc, setDoc } from 'firebase/firestore'
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
+import React, { useEffect, useState } from "react";
+import { auth, db } from "../firebase";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { collection, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const navigation = useNavigation()
+  const navigation = useNavigation();
 
-    // useEffect(() => {
-    //     onAuthStateChanged(auth, (user) => {
-    //         if (user) {
-    //             navigation.replace("Home")
-    //         }
-    //         // getDoc(doc(db, "Users", user.uid))
-    //         // .then((doc) => {
-    //         //     phone = doc.data().phone
-    //         // }
-    //     })
-    // }, [])
+  // useEffect(() => {
+  //     onAuthStateChanged(auth, (user) => {
+  //         if (user) {
+  //             navigation.replace("Home")
+  //         }
+  //         // getDoc(doc(db, "Users", user.uid))
+  //         // .then((doc) => {
+  //         //     phone = doc.data().phone
+  //         // }
+  //     })
+  // }, [])
 
-    const handleSignUp = () => {
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                setDoc(doc(db, "Users", user.uid), {
-                    email: user.email
-                })
-                navigation.replace("SignUp1")
-            })
-            .catch((error) => alert(error.message))
-    };
+  const handleSignUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setDoc(doc(db, "Users", user.uid), {
+          email: user.email,
+        });
+        navigation.replace("SignUp1");
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    const handleLogin = () => {
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                navigation.replace("Home")
-                // navigation.replace("SignUp1")
-            })
-            .catch((error) => alert(error.message))
-    };
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        navigation.replace("Home");
+        // navigation.replace("SignUp1")
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    return (
-        <KeyboardAvoidingView
-            style={styles.container}
-            behavior="padding"
-        >
-            <View style={styles.inputContainer}>
-                <TextInput
-                    placeholder="Email"
-                    value={email}
-                    onChangeText={text => setEmail(text)}
-                    style={styles.input}
-                />
-                <TextInput
-                    placeholder="Password"
-                    value={password}
-                    onChangeText={text => setPassword(text)}
-                    style={styles.input}
-                    secureTextEntry
-                />
-            </View>
+  return (
+    <View
+      style={styles.container}
+      behavior="padding">
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/burger.png")}
+        />
+      </View>
 
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity
-                    onPress={handleLogin}
-                    style={styles.button}
-                >
-                    <Text style={styles.buttonText}>Login</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={handleSignUp}
-                    style={[styles.button, styles.buttonOutline]}
-                >
-                    <Text style={styles.buttonOutlineText}>Sign Up</Text>
-                </TouchableOpacity>
-            </View>
-        </KeyboardAvoidingView>
-    )
-}
+      <KeyboardAvoidingView style={styles.inputContainer}>
+        <Text style={styles.heading3}>Chow anytime, chow now.</Text>
+        <Text style={styles.bodyText}>Log in to your Chow Now account.</Text>
 
-export default LoginScreen
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={[styles.input, styles.marginTopMedium]}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={[styles.input, styles.marginTopMedium]}
+          secureTextEntry
+        />
+      </KeyboardAvoidingView>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={handleLogin}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonOutline]}>
+          <Text style={styles.buttonOutlineText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default LoginScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    inputContainer: {
-        width: '80%'
-    },
-    input: {
-        backgroundColor: 'white',
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        borderRadius: 10,
-        marginTop: 5,
-    },
-    buttonContainer: {
-        width: '60%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 40,
-    },
-    button: {
-        backgroundColor: '#0782F9',
-        width: '100%',
-        padding: 15,
-        borderRadius: 10,
-        alignItems: 'center',
-    },
-    buttonOutline: {
-        backgroundColor: 'white',
-        marginTop: 5,
-        borderColor: '#0782F9',
-        borderWidth: 2,
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: '700',
-        fontSize: 16,
-    },
-    buttonOutlineText: {
-        color: '#0782F9',
-        fontWeight: '700',
-        fontSize: 16,
-    },
-})
+  container: {
+    flex: 1,
+    display: "flex",
+    margin: 20,
+    marginTop: 80,
+    justifyContent: "center",
+    gap: 40,
+    alignContent: "center",
+    fontFamily: "Karla-Regular",
+  },
+  imageContainer: { flex: 1 },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: "contain",
+  },
+  marginTopMedium: {
+    marginTop: 20,
+  },
+  inputContainer: { flex: 1, width: "100%", alignContent: "center" },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 20,
+    borderRadius: 10,
+    fontSize: 16,
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#F57C00",
+    width: "100%",
+    padding: 20,
+    borderRadius: 12,
+    alignItems: "center",
+  },
+  buttonOutline: {
+    backgroundColor: "transparent",
+    marginTop: 20,
+    borderColor: "#F57C00",
+    borderWidth: 1,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: "#F57C00",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  heading3: {
+    fontWeight: "700",
+    fontSize: 24,
+    paddingBottom: 12,
+    textAlign: "center",
+  },
+  bodyText: {
+    textAlign: "center",
+    fontSize: 16,
+    paddingBottom: 20,
+  },
+});
